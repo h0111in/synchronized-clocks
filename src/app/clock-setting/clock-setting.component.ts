24e70a5fe099e09ba849c1be8afcc8d9b7f7810c
Hosein Nourani <h.nourani@hotmail.com>
2020-08-09 11:12:45 -0400

    implemented structure / behavior

--- src/app/clock-setting/clock-setting.component.ts
+++ src/app/clock-setting/clock-setting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-clock-setting',
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockSettingComponent implements OnInit {
 
+  @Output() hourChanged = new EventEmitter<number>();
+  @Output() secondChanged = new EventEmitter<number>();
+  @Output() minuteChanged = new EventEmitter<number>();
+
+
+  @Input() hour: number;
+  @Input() minute: number;
+  @Input() second: number;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * onHourIncrease
+   */
+  public onHourIncrease() {
+    this.hourChanged.emit(this.hour + 1);
+  }
+  /**
+   * onHourDecrease
+   */
+  public onHourDecrease() {
+    this.hourChanged.emit(this.hour - 1);
+  }
+  /**
+   * onMinuteIncrease
+   */
+  public onMinuteIncrease() {
+    this.minuteChanged.emit(this.minute + 1);
+  }
+
+  /**
+   * onMinuteDecrease
+   */
+  public onMinuteDecrease() {
+    this.minuteChanged.emit(this.minute - 1);
+  }
+
+  /**
+   * onSecondIncrease
+   */
+  public onSecondIncrease() {
+    this.secondChanged.emit(this.second + 1);
+  }
+
+  /**
+   * onSecondDecrease
+   */
+  public onSecondDecrease() {
+    this.secondChanged.emit(this.second - 1);
   }
 
 }
