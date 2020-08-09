f0fae843cb21b4194388ab90af1abc835897ba31
Hosein Nourani <h.nourani@hotmail.com>
2020-08-09 18:11:14 -0400

    Revert "Clock core service"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnalogClockComponent } from './analog-clock/analog-clock.component';
-import { DigitalClockComponent } from './digital-clock/digital-clock.component';
-import { ClockSettingComponent } from './clock-setting/clock-setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AnalogClockComponent,
-    DigitalClockComponent,
-    ClockSettingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
